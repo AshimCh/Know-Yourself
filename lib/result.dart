47ca73faf515b4a0d384de71bdc97aa3a850f669
AshimCh <ashimchaudhary21st@gmail.com>
2021-09-20 11:09:21 +0545

    Know Yourself

---
+++ lib/result.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore >= 25) {
+      resultText = 'You are cute and innocent!';
+    } else if (resultScore >= 18) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore >= 12) {
+      resultText = 'You are strange?!';
+    } else {
+      resultText = 'You have a evil taste..haha';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text(
+              'Restart Quiz!',
+            ),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
+    );
+  }
+}
